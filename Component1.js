8add8777d6c8554701c08c18890d9c2275c358eb
Sneha B s <snehabs2200@gmail.com>
2025-05-31 12:26:33 +0530

    Create Component1.js

---
+++ Component1.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../styles/web.css';  // Make sure the correct path is used
+
+function Component1() {
+  return (
+    <div className="parent">
+      <div className="parent-content">
+        <h1>Distance Learning Education Center</h1>
+        <p>Learn from the comfort of your home</p>
+        <button className="get-start-btn">Get Start</button>
+      </div>
+    </div>
+  );
+}
+
+export default Component1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
